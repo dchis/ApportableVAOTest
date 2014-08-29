de851377abb795f7dd41c10b9302dd410a64c1e9
Chis Dan-Ionut <dan.ionut.chis@gmail.com>
2014-08-29 14:23:24 +0300

    Initial push.

---
+++ ApportableVAOTest/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  ApportableVAOTest
+//
+//  Created by Dan Chis on 8/29/14.
+//  Copyright tapptix 2014. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ ApportableVAOTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ApportableVAOTest
+//
+//  Created by Dan Chis on 8/29/14.
+//  Copyright tapptix 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
